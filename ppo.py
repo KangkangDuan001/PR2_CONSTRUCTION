bd5263fa58e9c1a5dd05c94dc62d4f3be4bd7e59
KangkangDuan001 <108002909+KangkangDuan001@users.noreply.github.com>
2023-04-09 16:36:16 -0700

    Add files via upload

--- ppo.py
+++ ppo.py
@@ -1,154 +1,96 @@
-# E. Culurciello
-# February 2021
-
-# PyBullet UR-5 from https://github.com/josepdaniel/UR5Bullet
-# PPO from: https://github.com/nikhilbarhate99/PPO-PyTorch/blob/master/PPO_continuous.py
-
 import torch
 import torch.nn as nn
-from torch.distributions import MultivariateNormal
-import gym
-import numpy as np
-
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-class Memory:
-    def __init__(self):
-        self.actions = []
-        self.states = []
-        self.logprobs = []
-        self.rewards = []
-        self.is_terminals = []
-    
-    def clear_memory(self):
-        del self.actions[:]
-        del self.states[:]
-        del self.logprobs[:]
-        del self.rewards[:]
-        del self.is_terminals[:]
+import torch.nn.functional as F
+import torch.optim as optim
 
 
-class ActorCritic(nn.Module):
-    def __init__(self, device, state_dim, emb_size, action_dim, action_std):
-        self.device = device
-        super(ActorCritic, self).__init__()
-        # action mean range -1 to 1
-        self.actor =  nn.Sequential(
-                nn.Linear(state_dim, emb_size),
-                nn.Tanh(),
-                nn.Linear(emb_size, emb_size),
-                nn.Tanh(),
-                # nn.Linear(emb_size, emb_size),
-                # nn.Tanh(),
-                nn.Linear(emb_size, action_dim),
-                nn.Tanh()
-                )
-        # critic
-        self.critic = nn.Sequential(
-                nn.Linear(state_dim, emb_size),
-                nn.Tanh(),
-                nn.Linear(emb_size, emb_size),
-                nn.Tanh(),
-                # nn.Linear(emb_size, emb_size),
-                # nn.Tanh(),
-                nn.Linear(emb_size, 1)
-                )
-        self.action_var = torch.full((action_dim,), action_std*action_std).to(self.device)
-        
-    def forward(self):
-        raise NotImplementedError
-    
-    def act(self, state, memory):
-        action_mean = self.actor(state)
-        cov_mat = torch.diag(self.action_var).to(self.device)
-        
-        distribution = MultivariateNormal(action_mean, cov_mat)
-        action = distribution.sample()
-        action_logprob = distribution.log_prob(action)
-        
-        memory.states.append(state)
-        memory.actions.append(action)
-        memory.logprobs.append(action_logprob)
-        
-        return action.detach()
-    
-    def evaluate(self, state, action):   
-        action_mean = self.actor(state)
-        
-        action_var = self.action_var.expand_as(action_mean)
-        cov_mat = torch.diag_embed(action_var).to(self.device)
-        
-        distribution = MultivariateNormal(action_mean, cov_mat)
-        
-        action_logprobs = distribution.log_prob(action)
-        distribution_entropy = distribution.entropy()
-        state_value = self.critic(state)
-        
-        return action_logprobs, torch.squeeze(state_value), distribution_entropy
+class PPO():
+    def __init__(self,
+                 actor_critic,
+                 clip_param,
+                 ppo_epoch,
+                 num_mini_batch,
+                 value_loss_coef,
+                 entropy_coef,
+                 lr=None,
+                 eps=None,
+                 max_grad_norm=None,
+                 use_clipped_value_loss=True):
 
+        self.actor_critic = actor_critic
 
+        self.clip_param = clip_param
+        self.ppo_epoch = ppo_epoch
+        self.num_mini_batch = num_mini_batch
 
-class PPO:
-    def __init__(self, args, env, arm):
-        self.args = args
-        self.env = env
-        self.device = self.args.device
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
 
-        self.state_dim = self.env.observation_space.shape[0]
-        self.action_dim = self.env.action_space.shape[0]/2
-        
-        self.policy = ActorCritic(self.device ,self.state_dim, self.args.emb_size, self.action_dim, self.args.action_std).to(self.device)
-        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=self.args.lr, betas=self.args.betas)
-        
-        self.policy_old = ActorCritic(self.device, self.state_dim, self.args.emb_size, self.action_dim, self.args.action_std).to(self.device)
-        self.policy_old.load_state_dict(self.policy.state_dict())
-        
-        self.MseLoss = nn.MSELoss()
-    
-    def select_action(self, state, memory):
-        state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
-        return self.policy_old.act(state, memory).cpu().data.numpy().flatten()
-    
-    def update(self, memory):
-        # Monte Carlo estimate of rewards:
-        rewards = []
-        discounted_reward = 0
-        for reward, is_terminal in zip(reversed(memory.rewards), reversed(memory.is_terminals)):
-            if is_terminal:
-                discounted_reward = 0
-            discounted_reward = reward + (self.args.gamma * discounted_reward)
-            rewards.insert(0, discounted_reward)
-        
-        # Normalizing the rewards:
-        rewards = torch.tensor(rewards).to(self.device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
-        rewards = rewards.float().squeeze()
-        
-        # convert list to tensor
-        old_states = torch.squeeze(torch.stack(memory.states).to(self.device), 1).detach()
-        old_actions = torch.squeeze(torch.stack(memory.actions).to(self.device), 1).detach()
-        old_logprobs = torch.squeeze(torch.stack(memory.logprobs), 1).to(self.device).detach()
-        
-        # Optimize policy for K epochs:
-        for _ in range(self.args.K_epochs):
-            # Evaluating old actions and values :
-            logprobs, state_values, distribution_entropy = self.policy.evaluate(old_states, old_actions)
-            
-            # Finding the ratio (pi_theta / pi_theta__old):
-            ratios = torch.exp(logprobs - old_logprobs.detach())
+        self.max_grad_norm = max_grad_norm
+        self.use_clipped_value_loss = use_clipped_value_loss
 
-            # Finding Surrogate Loss:
-            advantages = rewards - state_values.detach()   
-            surr1 = ratios * advantages
-            surr2 = torch.clamp(ratios, 1-self.args.eps_clip, 1+self.args.eps_clip) * advantages
-            loss = -torch.min(surr1, surr2) + \
-                    + self.args.loss_value_c*self.MseLoss(state_values, rewards) + \
-                    - self.args.loss_entropy_c*distribution_entropy
-            
-            # take gradient step
-            self.optimizer.zero_grad()
-            loss.mean().backward()
-            self.optimizer.step()
-            
-        # Copy new weights into old policy:
-        self.policy_old.load_state_dict(self.policy.state_dict())
+        self.optimizer = optim.Adam(actor_critic.parameters(), lr=lr, eps=eps)
+
+    def update(self, rollouts):
+        advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
+        advantages = (advantages - advantages.mean()) / (
+            advantages.std() + 1e-5)
+
+        value_loss_epoch = 0
+        action_loss_epoch = 0
+        dist_entropy_epoch = 0
+
+        for e in range(self.ppo_epoch):
+            if self.actor_critic.is_recurrent:
+                data_generator = rollouts.recurrent_generator(
+                    advantages, self.num_mini_batch)
+            else:
+                data_generator = rollouts.feed_forward_generator(
+                    advantages, self.num_mini_batch)
+
+            for sample in data_generator:
+                obs_batch, recurrent_hidden_states_batch, actions_batch, \
+                   value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
+                        adv_targ = sample
+
+                # Reshape to do in a single forward pass for all steps
+                values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
+                    obs_batch, recurrent_hidden_states_batch, masks_batch,
+                    actions_batch)
+
+                ratio = torch.exp(action_log_probs -
+                                  old_action_log_probs_batch)
+                surr1 = ratio * adv_targ
+                surr2 = torch.clamp(ratio, 1.0 - self.clip_param,
+                                    1.0 + self.clip_param) * adv_targ
+                action_loss = -torch.min(surr1, surr2).mean()
+
+                if self.use_clipped_value_loss:
+                    value_pred_clipped = value_preds_batch + \
+                        (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
+                    value_losses = (values - return_batch).pow(2)
+                    value_losses_clipped = (
+                        value_pred_clipped - return_batch).pow(2)
+                    value_loss = 0.5 * torch.max(value_losses,
+                                                 value_losses_clipped).mean()
+                else:
+                    value_loss = 0.5 * (return_batch - values).pow(2).mean()
+
+                self.optimizer.zero_grad()
+                (value_loss * self.value_loss_coef + action_loss -
+                 dist_entropy * self.entropy_coef).backward()
+                nn.utils.clip_grad_norm_(self.actor_critic.parameters(),
+                                         self.max_grad_norm)
+                self.optimizer.step()
+
+                value_loss_epoch += value_loss.item()
+                action_loss_epoch += action_loss.item()
+                dist_entropy_epoch += dist_entropy.item()
+
+        num_updates = self.ppo_epoch * self.num_mini_batch
+
+        value_loss_epoch /= num_updates
+        action_loss_epoch /= num_updates
+        dist_entropy_epoch /= num_updates
+
+        return value_loss_epoch, action_loss_epoch, dist_entropy_epoch
